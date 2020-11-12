f8d7af4f215d403e657c8d39f389ec691468ada7
Hasanjan731 <hasanjanyz731@gmail.com>
2020-11-11 21:56:46 -0600

    Project is already in remote Repo, committing more changes

--- src/Hello.java
+++ src/Hello.java
@@ -2,4 +2,10 @@ public class Hello {
 
     // more stuff
     // Mac and Windows users are happy to see commit dialog finally
+
+    // I have already published on Github.com
+    // Now I am ready to make more commits
+    // But do I need to share again?
+    // Absolutely NO
+    //It's time to commit and just push!!!!!
 }
